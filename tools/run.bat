76561fa9ed15108c9a1c76f9704719461331ce55
Stefan Popov <stefanpopov@list.ru>
2018-09-30 03:46:52 +0300

    Fix build file

--- tools/run.bat
+++ tools/run.bat
@@ -2,7 +2,11 @@
 cd ..
 git reset --hard 
 git pull
-appcmd stop sites "Web Dictionary"
+REM The following commands does not work due to lack of write permissions
+REM appcmd stop sites "Web Dictionary"
+REM appcmd start sites "Web Dictionary"
+REM You should use iisreset
+iisreset /stop
 dotnet publish -c Release -o c://inetpub/wwwroot/webdictionary
-appcmd start sites "Web Dictionary"
+iisreset /start
 EXIT 1
