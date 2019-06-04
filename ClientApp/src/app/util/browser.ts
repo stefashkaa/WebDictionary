3c110f3abd59f7ce04fc7c66aba7e9f47ef7fae3
Stefan Popov <stefanpopov@list.ru>
2019-06-04 18:29:36 +0300

    Modify browser checker

--- ClientApp/src/app/util/browser.ts
+++ ClientApp/src/app/util/browser.ts
@@ -1,45 +1,11 @@
 import * as _ from 'lodash';
-import { UAParser } from 'ua-parser-js';
-
-import { trace } from '../diagnostic/trace';
 
 export class Browser {
-    private isTabletCached: boolean;
     private isMobileCached: boolean;
-    private isIeCached: boolean;
-    private isIosCached: boolean;
-    private isJavaFxCached: boolean;
-    private browserCached: string;
-    private deviceCached: string;
-    private operatingSystemCached: string;
     private userAgent: string;
-
-    private readonly logger = trace.get('Browser');
 
     constructor() {
         this.userAgent = window.navigator.userAgent.toLowerCase();
-    }
-
-    public get browser(): string {
-        if (!this.browserCached) {
-            this.browserCached = new UAParser().getBrowser().name;
-        }
-        return this.browserCached;
-    }
-
-    public get device(): string {
-        if (!this.deviceCached) {
-            this.deviceCached = new UAParser().getDevice().model || 'PC';
-        }
-        return this.deviceCached;
-    }
-
-    public get operatingSystem(): string {
-        if (!this.operatingSystemCached) {
-            const osData = new UAParser().getOS();
-            this.operatingSystemCached = _.compact([osData.name, osData.version]).join(' ');
-        }
-        return this.operatingSystemCached;
     }
 
     public isMobile(): boolean {
@@ -50,68 +16,8 @@ export class Browser {
         return this.isMobileCached;
     }
 
-    public isIos(): boolean {
-        if (this.isIosCached == null) {
-            const iosRegex = /iPad|iPhone|iPod/i;
-            this.isIosCached = iosRegex.test(this.userAgent) && !(window as any).MSStream;
-        }
-        return this.isIosCached;
-    }
-
-    public isTablet(): boolean {
-        if (this.isTabletCached == null) {
-            const tabletRegex = /Tablet|iPad/i;
-            this.isTabletCached = tabletRegex.test(this.userAgent);
-        }
-        return this.isTabletCached;
-    }
-
-    public isIE(): boolean {
-        if (this.isIeCached == null) {
-            const ieUserAgentRegex = /msie|trident\/7/i;
-            this.isIeCached = ieUserAgentRegex.test(this.userAgent);
-        }
-        return this.isIeCached;
-    }
-
-    public isJavaFX(): boolean {
-        if (this.isJavaFxCached == null) {
-            const javaFxUserAgentRegex = /javafx/i;
-            this.isJavaFxCached = javaFxUserAgentRegex.test(this.userAgent);
-        }
-        return this.isJavaFxCached;
-    }
-
-    public logBrowserInformation() {
-        const timeZoneOffset = new Date().getTimezoneOffset();
-        let gmt = (-timeZoneOffset / 60).toString(10);
-        if (timeZoneOffset <= 0) {
-            gmt = `+${gmt}`;
-        }
-        const browserInfo = {
-            appCodeName: window.navigator.appCodeName,
-            appName: window.navigator.appName,
-            platform: window.navigator.platform,
-            vendor: window.navigator.vendor,
-            userAgent: window.navigator.userAgent,
-            cookieEnabled: window.navigator.cookieEnabled,
-            language: window.navigator.language,
-            timeZoneOffset: `${timeZoneOffset}min (GMT${gmt})`,
-            screen: `${screen.width}x${screen.height}`
-        };
-        this.logger.info('browser info', browserInfo);
-    }
-
-    public isInIframe(): boolean {
-        try {
-            return window.parent && window.top !== window.self;
-        } catch (e) {
-            return true;
-        }
-    }
-
     private getOrientationType(): string {
-        const obj = (<any>screen).orientation || (<any>screen).mozOrientation || screen.msOrientation;
+        const obj = (<any>screen).orientation || (<any>screen).mozOrientation || (<any>screen).msOrientation;
         return obj ? obj.type : null;
     }
 
